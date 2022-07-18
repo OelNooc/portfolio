d0cc7586b7999ec8462eb2a43f43d8d5844443e8
Leonardo Catejo <leo.catejoa@gmail.com>
2022-07-18 12:33:49 -0400

    Adapter, item layout, and proyect model added

---
+++ app/src/main/java/cl/nooc/portafolio/adapter/ProyectsAdapter.kt
@@ -0,0 +1,26 @@
+package cl.nooc.portafolio.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import cl.nooc.portafolio.databinding.ProyectsItemsBinding
+
+class ProyectsAdapter {
+
+/*    class CustomViewHolder(itemView: View, val listener: MiListener) : RecyclerView.ViewHolder(itemView)
+    {
+        private val binding = ProyectsItemsBinding.bind(itemView)
+
+        fun bindData(game: GamesItem)
+        {
+            with(binding)
+            {
+                Picasso.get().load(game.background_image).resize(250,250).into(ivGame)
+                tvNameGame.text = game.name
+                tvGameOut.text = game.released
+                itemView.setOnClickListener {
+                    listener.miOnClick(game)
+                }
+            }
+        }
+    }*/
+}
---
+++ app/src/main/java/cl/nooc/portafolio/modelo/Proyect.kt
@@ -0,0 +1,9 @@
+package cl.nooc.portafolio.modelo
+
+class Proyect(
+    val imagen: Int,
+    val nombre: String,
+    val descripcion: String
+) {
+
+}
