a8675f250974e056e41f337bdb766a8c4e497378
Leonardo Catejo <leo.catejoa@gmail.com>
2022-07-20 12:59:07 -0400

    Finish Adapter

--- app/src/main/java/cl/nooc/portafolio/adapter/ProyectsAdapter.kt
+++ app/src/main/java/cl/nooc/portafolio/adapter/ProyectsAdapter.kt
@@ -1,26 +1,60 @@
 package cl.nooc.portafolio.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import cl.nooc.portafolio.R
 import cl.nooc.portafolio.databinding.ProyectsItemsBinding
+import cl.nooc.portafolio.modelo.Proyect
 
-class ProyectsAdapter {
+class ProyectsAdapter: RecyclerView.Adapter<ProyectsAdapter.CustomViewHolder>() {
 
-/*    class CustomViewHolder(itemView: View, val listener: MiListener) : RecyclerView.ViewHolder(itemView)
+    var lista = ArrayList<Proyect>()
+    lateinit var listener: MiListener
+
+    class CustomViewHolder(itemView: View, val listener: MiListener) : RecyclerView.ViewHolder(itemView)
     {
         private val binding = ProyectsItemsBinding.bind(itemView)
 
-        fun bindData(game: GamesItem)
+        fun bindData(proyect: Proyect)
         {
             with(binding)
             {
-                Picasso.get().load(game.background_image).resize(250,250).into(ivGame)
-                tvNameGame.text = game.name
-                tvGameOut.text = game.released
+                ivIProyecto.setImageResource(proyect.imagen)
+                tvNProyecto.text = proyect.nombre
+                tvDProyecto.text = proyect.descripcion
                 itemView.setOnClickListener {
-                    listener.miOnClick(game)
+                    listener.miOnClick(proyect)
                 }
             }
         }
-    }*/
+    }
+
+    interface MiListener{
+        fun miOnClick(proyect: Proyect)
+    }
+
+    fun setMiListener(listener: MiListener)
+    {
+        this.listener = listener
+    }
+
+    fun updateData(proyects: ArrayList<Proyect>)
+    {
+        lista = proyects
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.proyects_items,
+        parent, false)
+        return CustomViewHolder(view, listener)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        holder.bindData(lista[position])
+    }
+
+    override fun getItemCount(): Int = lista.size
 }
